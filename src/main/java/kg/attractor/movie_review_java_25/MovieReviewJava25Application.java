795becbab144ba5e25da399e6baadcd561f1059e
Aleksei Podstavin <gwinbleid86@gmail.com>
2025-07-08 19:42:38 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/movie_review_java_25/MovieReviewJava25Application.java
@@ -0,0 +1,13 @@
+package kg.attractor.movie_review_java_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewJava25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewJava25Application.class, args);
+	}
+
+}
---
+++ src/test/java/kg/attractor/movie_review_java_25/MovieReviewJava25ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.movie_review_java_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewJava25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
